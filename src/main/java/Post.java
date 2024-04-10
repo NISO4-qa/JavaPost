7e4d46aaf3647652620bc78240afd703b4eaeaac
Nikita Chalyi <nikita4556@gmail.com>
2024-04-10 23:14:04 +0300

    First commit

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,6 +1,6 @@
 public class Post {
     public FormDate birthday;
-    public String name; // поле для имени
+    public String name;
     public String passport;
     public String patronymic;
     public String phone;
